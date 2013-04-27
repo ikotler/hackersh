a6000712a05867d2ac54dcfad0d7a0b05e69c829
Itzik Kotler <xorninja@gmail.com>
2013-04-27 14:38:36 +0300

    Implement domain - new root component for processing domain names

---
+++ hackersh/components/internal/domain.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2013 Itzik Kotler
+#
+# This file is part of Hackersh.
+#
+# Hackersh is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# Hackersh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hackersh; see the file COPYING.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+
+# Local imports
+
+import hackersh.objects
+
+
+# Metadata
+
+__author__ = "Itzik Kotler <xorninja@gmail.com>"
+__version__ = "0.1.0"
+
+
+# Implementation
+
+class Domain(hackersh.objects.RootComponent):
+
+    def run(self, argv, context):
+
+        _context = False
+
+        if argv[0].find('.') != -1:
+
+            _context = hackersh.objects.RemoteSessionContext(DOMAIN=argv[0])
+
+        return _context
