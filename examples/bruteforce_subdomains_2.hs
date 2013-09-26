1ff8232dd4576100fb72ea8c01b56abe912db832
Itzik Kotler <xorninja@gmail.com>
2013-09-26 18:51:32 +0300

    Add examples/ dir

---
+++ examples/bruteforce_subdomains_2.hs
@@ -0,0 +1,21 @@
+#! /usr/bin/env hackersh
+#
+# Copyright (C) 2013 Itzik Kotler
+#
+# This file is part of Hackersh.
+#
+# Hackersh is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# Hackersh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hackersh; see the file COPYING.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+"localdomain" -> domain -> dnsdict6 -4 -> print_all
---
+++ examples/ping_sweep_2.hs
@@ -0,0 +1,22 @@
+#! /usr/bin/env hackersh
+#
+# Copyright (C) 2013 Itzik Kotler
+#
+# This file is part of Hackersh.
+#
+# Hackersh is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# Hackersh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hackersh; see the file COPYING.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+"127.0.0.1/8" -> ipv4_range -> ping -> print_all
+
